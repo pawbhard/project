43eafd4fe98dd6236d44352095f7b43472cb2ead
vishukha <vishukha@github.com>
2015-10-15 20:18:56 +0530

    tracker code

--- server/server_tracker.cpp
+++ server/server_tracker.cpp
@@ -1,5 +1,5 @@
 #include "server_tracker.h"
-#include <list.h>
+//#include <list.h>
 //using std::make_pair;
  
 int track_data::set_track(const int task_id, int client_id, int s, int e) {
@@ -16,7 +16,7 @@ int track_data::set_track(const int task_id, int client_id, int s, int e) {
  
 // to delete all client-id in one shot  
 int track_data::delete_all_track(int task_id) {
-    unordered_map<int, position>::iterator it = track.find(task_id);
+    unordered_map<int, list <position> >::iterator it = track.find(task_id);
     if(it == track.end()) {
         DATABASE_DEBUG("Task not found");
         return FAILURE;
@@ -27,19 +27,33 @@ int track_data::delete_all_track(int task_id) {
 
 // to delete a particular client-id
 int track_data::delete_track(int task_id, int *client_id) {
-    unordered_map<int, position>::iterator it = track.find(task_id);
+    unordered_map<int, list <position> >::iterator it = track.find(task_id);
     if(it == track.end()) {
         DATABASE_DEBUG("Task not found");
         return FAILURE;
     }
     list <position> client_list = track[task_id];
     position p;
-    for (std::list<int>::iterator l_it=client_list.begin(); l_it != client_list.end(); ++l_it) {
+    for (list<position>::iterator l_it=client_list.begin(); l_it != client_list.end(); ++l_it) {
         p = *l_it;
-        if(p.client_id == *client_id) 
-            break;
+        if(p.client_id == *client_id) { 
+           client_list.erase(l_it);
+	   break;
+         }
+        
     }
-    my_list.remove(p);
     track[task_id] = client_list;
     return SUCCESS;
-}  
+} 
+
+int track_data::set_group_task_map(int task_id , int group_id) {
+	group_task_map[task_id] = group_id;
+        group_status[group_id] = 1;
+	return SUCCESS;
+} 
+
+void track_data::release_group(int task_id) {
+	group_status[group_task_map[task_id]] = 0;
+	group_task_map[task_id] = -1;
+}
+
--- server/server_tracker.h
+++ server/server_tracker.h
@@ -1,3 +1,17 @@
+#include<unordered_map>
+#include<cstdio>
+#include<iostream>
+#include<netinet/in.h>
+//#include<stdlib.h>
+#include <list>
+#define DATABASE_ERROR(format,args...) printf("DATABASE ERROR %s(%d): " format "\n" ,__FUNCTION__,__LINE__,##args)
+#define DATABASE_DEBUG(format,args...) printf("DATABASE DEBUG %s(%d): " format "\n" ,__FUNCTION__,__LINE__,##args)
+
+#define FAILURE 0
+#define SUCCESS 1
+
+using namespace std;
+
 typedef struct {
     int client_id;
     int start;
@@ -7,6 +21,8 @@ typedef struct {
 class track_data {
     private :
         unordered_map <int, list <position> > track;
+        unordered_map < int, int > group_task_map;
+	unordered_map < int, int> group_status; 
         track_data()
         {
             DATABASE_DEBUG("Created Object of track_data");
@@ -22,9 +38,11 @@ class track_data {
             if (!td) td = new track_data;
             return td;
         }   
-        int set_track(int task_id, int client_id, int s, int e); 
+        int set_track(int task_id, int client_id, int s, int e);
+        int set_group_task_map (int task_id , int group_id); 
         int delete_track(int task_id, int *client_id);
         int delete_all_track(int task_id);
+        void release_group (int task_id);
          
 };
 
